9c955989ef259b34e7ad8fc31c30cf242813a4c3
falali <falali@wacai.com>
2016-03-30 14:40:49 +0800

    更新demo

--- demo/app.js
+++ demo/app.js
@@ -23,7 +23,8 @@ var router = require('./router');
 app.set('views', path.join(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
-app.use(favicon(path.join(__dirname, 'favicon.ico')));
+// uncomment after placing your favicon in /src
+app.use(favicon(path.join(__dirname, 'src', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -42,6 +43,8 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+// error handlers
 
 // development error handler
 // will print stacktrace
